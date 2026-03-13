037adb7b1309cb32bc7a0d3701e3166a90ff8b57
FRnCDR <franyerifallasnichols@gmail.com>
2026-03-13 12:16:25 -0600

    Get de Categoria y SubCategoria

---
+++ ProductosAPI/BD/dbo/Stored Procedures/ObtenerSubCategorias.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[ObtenerSubCategorias]
+	@IdCategoria UNIQUEIDENTIFIER
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Consultar las subcategorías para el IdCategoria proporcionado
+    SELECT 
+        sc.Id,
+        sc.Nombre AS SubCategoria,
+        sc.IdCategoria
+    FROM dbo.SubCategorias sc
+    WHERE sc.IdCategoria = @IdCategoria;
+END
