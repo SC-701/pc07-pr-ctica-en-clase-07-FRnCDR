254b47d81c1df6f64575e7864c9d7709ed02620e
FRnCDR <franyerifallasnichols@gmail.com>
2026-03-13 09:18:00 -0600

    Base para comenzar con la practica

---
+++ ProductosAPI/BD/dbo/Stored Procedures/ObtenerProducto.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[ObtenerProducto]
+	-- Add the parameters for the stored procedure here
+    @Id as UNIQUEIDENTIFIER
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT 
+      p.Id,
+      p.Nombre AS Producto,
+      p.Descripcion,
+      p.Precio,
+      p.Stock,
+      p.CodigoBarras,
+      sc.Nombre AS SubCategoria
+FROM dbo.Producto p
+INNER JOIN dbo.SubCategorias sc 
+    ON p.IdSubCategoria = sc.Id
+  WHERE p.Id = @Id;
+END
---
+++ ProductosAPI/BD/dbo/Stored Procedures/ObtenerProductos.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[ObtenerProductos]
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT 
+      p.Id,
+      p.Nombre AS Producto,
+      p.Descripcion,
+      p.Precio,
+      p.Stock,
+      p.CodigoBarras,
+      sc.Nombre AS SubCategoria
+FROM dbo.Producto p
+INNER JOIN dbo.SubCategorias sc 
+    ON p.IdSubCategoria = sc.Id
+
+  
+END
