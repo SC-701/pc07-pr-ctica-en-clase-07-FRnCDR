19b4b396926e779ba4b3b869f993c0da59164147
FRnCDR <franyerifallasnichols@gmail.com>
2026-03-17 09:06:56 -0600

    Arreglo de sp

--- ProductosAPI/BD/dbo/Stored Procedures/ObtenerProducto.sql
+++ ProductosAPI/BD/dbo/Stored Procedures/ObtenerProducto.sql
@@ -1,28 +1,22 @@
-﻿-- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE [dbo].[ObtenerProducto]
-	-- Add the parameters for the stored procedure here
-    @Id as UNIQUEIDENTIFIER
+﻿CREATE PROCEDURE [dbo].[ObtenerProducto]
+    @Id AS UNIQUEIDENTIFIER
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
-	SELECT 
-      p.Id,
-      p.Nombre AS Producto,
-      p.Descripcion,
-      p.Precio,
-      p.Stock,
-      p.CodigoBarras,
-      sc.Nombre AS SubCategoria
-FROM dbo.Producto p
-INNER JOIN dbo.SubCategorias sc 
-    ON p.IdSubCategoria = sc.Id
-  WHERE p.Id = @Id;
+    SELECT 
+        p.Id,
+        p.Nombre AS Nombre,
+        p.Descripcion,
+        p.Precio,
+        p.Stock,
+        p.CodigoBarras,
+        sc.Nombre AS SubCategoria,
+        c.Nombre AS Categoria
+    FROM dbo.Producto p
+    INNER JOIN dbo.SubCategorias sc 
+        ON p.IdSubCategoria = sc.Id
+    INNER JOIN dbo.Categorias c
+        ON sc.IdCategoria = c.Id
+    WHERE p.Id = @Id;
 END
--- ProductosAPI/BD/dbo/Stored Procedures/ObtenerProductos.sql
+++ ProductosAPI/BD/dbo/Stored Procedures/ObtenerProductos.sql
@@ -1,28 +1,20 @@
-﻿-- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE [dbo].[ObtenerProductos]
-	-- Add the parameters for the stored procedure here
+﻿CREATE PROCEDURE [dbo].[ObtenerProductos]
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
-SELECT 
-      p.Id,
-      p.Nombre AS Producto,
-      p.Descripcion,
-      p.Precio,
-      p.Stock,
-      p.CodigoBarras,
-      sc.Nombre AS SubCategoria
-FROM dbo.Producto p
-INNER JOIN dbo.SubCategorias sc 
-    ON p.IdSubCategoria = sc.Id
-
-  
+    SELECT 
+        p.Id,
+        p.Nombre AS Nombre,
+        p.Descripcion,
+        p.Precio,
+        p.Stock,
+        p.CodigoBarras,
+        sc.Nombre AS SubCategoria,
+        c.Nombre AS Categoria
+    FROM dbo.Producto p
+    INNER JOIN dbo.SubCategorias sc 
+        ON p.IdSubCategoria = sc.Id
+    INNER JOIN dbo.Categorias c
+        ON sc.IdCategoria = c.Id
 END
